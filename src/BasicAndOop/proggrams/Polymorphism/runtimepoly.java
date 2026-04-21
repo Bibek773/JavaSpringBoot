d108b0eb03c53a33e857fcd499d579dd45f673d1
Bibek Ghimire <bibekghimire773@gmail.com>
2026-04-21 22:03:50 +0545

    Feat: added demo program of runtime polymorphism

---
+++ src/BasicAndOop/proggrams/Polymorphism/runtimepoly.java
@@ -0,0 +1,35 @@
+package BasicAndOop.proggrams.Polymorphism;
+
+
+
+class Animal {
+    void sound() {
+        System.out.println("Animal makes a sound");
+    }
+}
+
+class Dog extends Animal {
+    @Override
+    void sound() {
+        System.out.println("Dog barks");
+    }
+}
+
+class Cat extends Animal {
+    @Override
+    void sound() {
+        System.out.println("Cat meows");
+    }
+}
+
+public class runtimepoly {
+    public static void main(String[] args) {
+        Animal a;
+
+        a = new Dog();  // Upcasting
+        a.sound();      // Calls Dog's sound()
+
+        a = new Cat();
+        a.sound();      // Calls Cat's sound()
+    }
+}
