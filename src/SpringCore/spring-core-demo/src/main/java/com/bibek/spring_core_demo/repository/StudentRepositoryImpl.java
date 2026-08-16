29a1da5c5a9f2de3ad61032447ac9e1a6beac77a
Bibek Ghimire <bibekghimire773@gmail.com>
2026-08-16 23:30:58 +0545

    feat: created implementation for interface StudentRepository for CRUD

---
+++ src/SpringCore/spring-core-demo/src/main/java/com/bibek/spring_core_demo/repository/StudentRepositoryImpl.java
@@ -0,0 +1,51 @@
+package com.bibek.spring_core_demo.repository;
+
+import com.bibek.spring_core_demo.model.Student;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class StudentRepositoryImpl implements StudentRepository {
+
+    private final List<Student> students = new ArrayList<>();
+
+    @Override
+    public void save(Student student) {
+        students.add(student);
+    }
+
+    @Override
+    public List<Student> findAll() {
+        return students;
+    }
+
+    @Override
+    public Student findById(int idNumber) {
+        for (Student student : students) {
+            if (student.getIdNumber() == idNumber) {
+                return student;
+            }
+        }
+        return null;
+    }
+
+    @Override
+    public void deleteById(int idNumber) {
+        for (Student student : students) {
+            if (student.getIdNumber() == idNumber) {
+                students.remove(student);
+                return;
+            }
+        }
+    }
+
+    @Override
+    public void update(Student student) {
+        for (int i = 0; i < students.size(); i++) {
+            if (students.get(i).getIdNumber() == student.getIdNumber()) {
+                students.set(i, student);
+                return;
+            }
+        }
+    }
+}
