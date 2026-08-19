bd90f92ad419aebacce42b7a24c10c1d13cdb9a0
Bibek Ghimire <bibekghimire773@gmail.com>
2026-08-19 22:09:50 +0545

    feat: Handeling techer data added

---
+++ SpringCore/spring-core-demo/src/main/java/com/bibek/spring_core_demo/service/TeacherServices.java
@@ -0,0 +1,32 @@
+package com.bibek.spring_core_demo.service;
+
+import com.bibek.spring_core_demo.model.Teacher;
+import com.bibek.spring_core_demo.repository.TeacherRepository;
+
+import java.util.List;
+
+public class TeacherServices {
+
+    private final TeacherRepository teacherRepository;
+
+    public TeacherServices(TeacherRepository teacherRepository) {
+        this.teacherRepository = teacherRepository;
+
+    }
+    public void saveTeacher(Teacher teacher){
+        teacherRepository.save(teacher);
+    }
+    public List<Teacher> getAllTeachers(){
+        return teacherRepository.findAll();
+
+    }
+    public Teacher getTeacherById( int id){
+        return teacherRepository.findById(id);
+    }
+    public void deleteTeacherById(int id){
+        teacherRepository.deleteById(id);
+    }
+    public void updateTeacher(Teacher teacher){
+        teacherRepository.update(teacher);
+    }
+}
